4b5fe656808e4d45e1a8cb756944e6e12ea7e1e3
Addiel Juarez <addieljuarez@gmail.com>
2012-05-16 12:30:29 -0500

    corregido y probado en device

--- Resources/app.js
+++ Resources/app.js
@@ -21,21 +21,22 @@ var winGeo = Titanium.UI.createWindow({
 //
 // create map
 //
-var mapView = Titanium.Map.createView({
-	mapType:Titanium.Map.STANDARD_TYPE,
-	//mapType:Titanium.Map.SATELLITE_TYPE,
-	//mapType:Titanium.Map.HYBRID_TYPE,
-	//region:{latitudeDelta:0.05, longitudeDelta: 0.05},
-	
-	regionFit:true,
-	animate:true,
-	userLocation:true,
-});
-//
-// add map to window
-//
-winGeo.add(mapView);
 
+var mapView = Titanium.Map.createView({
+		mapType:Titanium.Map.STANDARD_TYPE,
+		//mapType:Titanium.Map.SATELLITE_TYPE,
+		//mapType:Titanium.Map.HYBRID_TYPE,
+		//region:{latitude:latitude, longitude:longitude, latitudeDelta:0.05, longitudeDelta: 0.05},
+		
+		regionFit:true,
+		animate:true,
+		userLocation:true,
+		//annotations:[nombrePunto],
+	});
+	//
+	// add map to window
+	//
+	winGeo.add(mapView);
 
 //
 // geo
@@ -52,21 +53,24 @@ Titanium.Geolocation.getCurrentPosition(function(e){
 	var latitude = e.coords.latitude;
 	var altitude = e.coords.altitude;
 	var heading = e.coords.heading;
-	var acuracy= e.coords.accuracy;
+	var accuracy= e.coords.accuracy;
 	var timestamp = e.coords.timestamp;
-	var altitudeAccuracy = e.coord.altitudeAccuracy;
+//	var altitudeAccuracy = e.coord.altitudeAccuracy;
 	
-	mapView.region = {latitude: latitude, longitude : longitude, latitudeDelta:0.05, longitudeDelta: 0.05};
+	mapView.region = {latitude: latitude, longitude : longitude, latitudeDelta:0.01, longitudeDelta: 0.01};
 	//Ti.API.info('inicia la localización ' + e.latitude + 'latitud '+ e.longitude + ' longitud' );
-	var nombrePunto = Titanium.Map.createAnnotation({
-		latitude:e.latitude,
-		longitude: e.longitude,
-		title: 'usted está aqui',
-		subtitle: txtStartLocation.value,
-		animate:true,
-		pincolor:Titanium.Map.ANNOTATION_RED,
-	});
-	mapView.addAnnotation(nombrePunto)
+	// var nombrePunto = Titanium.Map.createAnnotation({
+		// latitude:e.latitude,
+		// longitude: e.longitude,
+		// title: 'usted está aqui',
+	// //	subtitle: txtStartLocation.value,
+		// animate:true,
+		// pincolor:Titanium.Map.ANNOTATION_RED,
+	// });
+	// //mapView.addAnnotation(nombrePunto);
+	
+		
+		
 });
 
 
